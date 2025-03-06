b96056abf07c5e2e0836719957c71da5272d6dcf
ActiveTutorial <111246057+ActiveTutorial@users.noreply.github.com>
2025-03-06 10:57:25 +0100

    Update prompts.js
    Cleaner version to store the prompts

--- prompts.js
+++ prompts.js
@@ -1,27 +1,36 @@
 function getPrompt(type, ...args) {
     switch (type) {
         case "normal": // Most confirmed prompt, see IC discord, probably the real one
-            return `Fire + Water = Steam
-Human + Robe = Judge
-Earth + Water = Plant
-Cow + Fire = Steak
-King + Ocean = Poseidon
-${args[0]} + ${args[1]} =`;
+            return [
+                "Fire + Water = Steam",
+                "Human + Robe = Judge",
+                "Earth + Water = Plant",
+                "Cow + Fire = Steak",
+                "King + Ocean = Poseidon",
+                `${args[0]} + ${args[1]} =`
+            ].join("\n");
+
         case "self": // Only Boulder is confirmed, but the first two are from Adam and seem real.
-            return `Wind + Wind = Tornado
-Water + Water = Lake
-Earth + Earth = Mountain
-Rock + Rock = Boulder
-${args[0]} + ${args[1]} =`;
+            return [
+                "Wind + Wind = Tornado",
+                "Water + Water = Lake",
+                "Earth + Earth = Mountain",
+                "Rock + Rock = Boulder",
+                `${args[0]} + ${args[1]} =`
+            ].join("\n");
+
         case "emoji": // This one is entirely made up; Neal doesn’t use together.ai for emojis.
-            return `Hat = 🎩
-Key = 🔑
-Bicycle = 🚲
-Spicy = 🌶️
-Clean = 🧼
-Glasses = 💶
-Eat = 🍽️
-${args[0]} =`;
+            return [
+                "Hat = 🎩",
+                "Key = 🔑",
+                "Bicycle = 🚲",
+                "Spicy = 🌶️",
+                "Clean = 🧼",
+                "Glasses = 💶",
+                "Eat = 🍽️",
+                `${args[0]} =`
+            ].join("\n");
+
         default:
             return null;
     }
